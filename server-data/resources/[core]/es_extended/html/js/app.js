f873effcf1ee25102716f68ebf08c11badbd8a03
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-06-17 17:26:24 +0200

    feat: es_extended core main loader for esx servers
    - Core loading for esx based servers

---
+++ server-data/resources/[core]/es_extended/html/js/app.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+(() => {
+	ESX = {};
+
+	ESX.inventoryNotification = function(add, label, count) {
+		let notif = '';
+
+		if (add) {
+			notif += '+';
+		}
+		else {
+			notif += '-';
+		}
+
+		if (count) {
+			notif += count + ' ' + label;
+		}
+		else {
+			notif += ' ' + label;
+		}
+
+		const elem = $('<div>' + notif + '</div>');
+		$('#inventory_notifications').append(elem);
+
+		$(elem)
+			.delay(3000)
+			.fadeOut(1000, function() {
+				elem.remove();
+			});
+	};
+
+	window.onData = (data) => {
+		if (data.action === 'inventoryNotification') {
+			ESX.inventoryNotification(data.add, data.item, data.count);
+		}
+	};
+
+	window.onload = function(e) {
+		window.addEventListener('message', (event) => {
+			onData(event.data);
+		});
+	};
+})();

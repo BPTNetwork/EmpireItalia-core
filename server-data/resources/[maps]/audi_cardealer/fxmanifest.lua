51bf619006150f6d11cac73c94e33a6055f09a9b
bitpredator <67551273+bitpredator@users.noreply.github.com>
2025-06-20 21:38:15 +0200

    audi map loading + basic ipl

---
+++ server-data/resources/[maps]/audi_cardealer/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version("cerulean")
+game("gta5")
+
+author("BPTNetwork")
+description("concessionaria audi italia")
+version("1.0.0")
+
+files({
+    "stream/*.yft",
+    "stream/*.ydr",
+    "stream/*.ytd",
+    "stream/*.ymf",
+})
